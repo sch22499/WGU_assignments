5278a1c43b27f3e19484e90809bd6e1182f00a73
sch22499 <sch22499@gmail.com>
2021-01-29 16:14:35 -0700

    thing

--- c195/src/sample/ViewController/Schedule.java
+++ c195/src/sample/ViewController/Schedule.java
@@ -15,13 +15,17 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import sample.DAO.AppointmentDAOImpl;
+import sample.DAO.CustomerDAOImpl;
 import sample.Main;
 import sample.Model.Appointment;
+import sample.Model.Customer;
 import sample.Model.User;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Schedule implements Initializable {
@@ -30,6 +34,8 @@ public class Schedule implements Initializable {
     private User currentUser;
 
     private static ObservableList<Appointment> allAppointments;
+
+    private static ObservableList<Customer> allCustomers;
 
     private  static Appointment selectedAppointment;
 
@@ -86,34 +92,116 @@ public class Schedule implements Initializable {
         window.setScene(scene);
     }
 
+    public String getCustomerName(int customerID){
+
+       String name = new String();
+
+        for (Customer customer : allCustomers) {
+            if(customer.getCustomerId() == customerID)
+                name = customer.getCustomerName();
+
+        }
+       return name;
+    }
+
 
 
     private void populateTable(ObservableList<Appointment> appointments) throws Exception {
 
+//todo LAMBDA TO POPULATE TABLES
 
         dateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStartTime().toString()));
         timeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStartTime().toString()));
         typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getType()));
-        customerColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getCustomerID()));
+        customerColumn.setCellValueFactory(cellData -> new SimpleStringProperty(getCustomerName(cellData.getValue().getCustomerID())));
         durationColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEndTime().toString()));
         table.setItems(appointments);
 
 
     }
 
+    public String getDayOfWeek(int value){
+        String day = "";
+        switch(value){
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thursday";
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+        }
+        return day;
+    }
+
+
+    public String getDate(Calendar date, boolean isDate){
+
+        String dayOfWeek = "";
+
+
+        Date date1 = date.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+
+        String dateString = "";
+        dateString = format.format(date1);
+        System.out.println(dateString);
+        dayOfWeek = getDayOfWeek(date.get(Calendar.DAY_OF_WEEK));
+        System.out.println(dayOfWeek);
+        if(isDate == true){
+            return dateString;
+        }else {
+
+            return dayOfWeek;
+        }}
+
     //TODO how should we format this data for the thingy
-    public static ObservableList<Appointment> getAppointmentsByMonth() throws Exception {
+    public  ObservableList<Appointment> getAppointmentsByMonth() throws Exception {
 
         ObservableList<Appointment> appointmentsByMonth = FXCollections.observableArrayList();
 
 
         allAppointments.forEach((Appointment appointment) -> {
+            getDate(appointment.getStartTime(), true);
             if (appointment.getStartTime().get(Calendar.MONTH) == selectedMonth) {
                 appointmentsByMonth.add(appointment);
             }
+
         });
         return appointmentsByMonth;
 
+    }
+
+    public ObservableList<Appointment> getAppointmnetsByWeekNumber(int weekNum){
+        ObservableList<Appointment> appointmentsByWeekNum = FXCollections.observableArrayList();
+
+        allAppointments.forEach((Appointment appointment) ->{
+            int appWeekNum = appointment.getStartTime().get(Calendar.WEEK_OF_MONTH);
+            if(appWeekNum == weekNum){
+                appointmentsByWeekNum.add(appointment);
+            }
+        });
+
+        return appointmentsByWeekNum;
+    }
+
+    private void getSelectedWeek(ChoiceBox<String> comboBox){
+
+        
     }
 
     private void getSelectedMonth(ChoiceBox<String> comboBox) throws Exception {
@@ -163,7 +251,10 @@ public class Schedule implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         selectedMonth = 11;
+
         try {
+            allCustomers = CustomerDAOImpl.getAllCustomers();
+
             allAppointments = AppointmentDAOImpl.getAllAppointments();//AppointmentDAOImpl.getAppointmentsForUser(currentUser.getUserid());
         } catch (Exception e) {
             e.printStackTrace();
@@ -173,7 +264,6 @@ public class Schedule implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         monthDropdown.getItems().addAll("January", "February","March","April","May","June","July","August","September","October","November","December");
 
         //TODO lambda
